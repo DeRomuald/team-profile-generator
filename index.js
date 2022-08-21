98c378f44109460f3943afe4aba7432a46228438
DeRomuald <deromuald.romulus@gmail.com>
2022-08-21 03:20:02 -0400

    page-template and generate page updates

--- index.js
+++ index.js
@@ -10,27 +10,28 @@ const {writeFile, copyFile} = require('./src/generate-page');
 
 const dataArr = [];
 //
-const engineerQuestions = [
+const managerQuestions = [
     {
         type: 'input',
         name: 'name',
-        message: "What is the Engineer's name?",
+        message: "What is the team manager's name?",
         validate: (input) => {
             if (input !== "") {
                 return true;
             }
             else {
-                return 'Name required!';
+                return "Please enter the name";
             }
         }
     },
     {
         type: 'input',
         name: 'id',
-        message: "What is the Engineer's id?",
+        message: "What is the team manager's id?",
         validate: (input) => {
+            // to make sure input is number and not an empty string
             if (isNaN(parseInt(input))) {
-                return 'ID number required!';
+                return 'Please enter an id number';
             }
             return true;
         }
@@ -38,174 +39,194 @@ const engineerQuestions = [
     {
         type: 'input',
         name: 'email',
-        message: "What is the Engineer's email?",
+        message: "What is the team manager's email?",
         validate: (email) => {
             let valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
             if (valid) {
                 return true;
             }
-            return 'Email address required!';
-        }
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: "What is the Engineer's github?",
-        validate: (input) => {
-            return (!input) ? 'Github required!' : true;
-        }
-    },
-];
-
-//
-const internQuestions =[
-    {
-        type: 'input',
-        name: 'name',
-        message: "What is the intern's name?",
-        validate: (input) => {
-            return (!input) ? 'Name required!' : true;
-        }
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "What is the intern's id#?",
-        validate: (input) => {
-            if (isNaN(parseInt(input))) {
-                return 'ID number required!';
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What is intern's email address?",
-        validate: (email) => {
-            let valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-            if (valid) {
-                return true;
-            }
-            return 'Email address required!';
-        }
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: "What school does the intern attend?",
-        validate: (input) => {
-            return (!input) ? 'School name required!' : true;
-        }
-    },
-];
-//
-const managerQuestions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: "What is the manager's name?",
-        validate: (input) => {
-            return (!input) ? 'Name required!' : true;
-        }
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "What is the manager's id#?",
-        validate: (input) => {
-            if (isNaN(parseInt(input))) {
-                return 'ID number required!';
-            }
-            return true;
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What is the manager's email address?",
-        validate: (email) => {
-            let valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-            if (valid) {
-                return true;
-            }
-            return 'Email address required!';
+            return "Please enter a valid email";
         }
     },
     {
         type: 'input',
         name: 'officeNumber',
-        message: "What is the manager's office number?",
+        message: "What is the team manager's office number?",
         validate: (input) => {
             if (isNaN(parseInt(input))) {
-                return 'Office number required!';
+                return 'Please enter an office number';
             }
             return true;
         }
     },
 ];
-//
-const addTeamMember = [
+
+// question for next step
+const addTeamOrFinishQuestion = [
     {
         type: 'list',
-        name: 'addTeamMember',
-        choices: ['engineer','intern','manager','done'],
-        messsage: 'Would you like to add another teammate?',
-
+        name: 'choice',
+        choices: ['Engineer', 'Intern', 'Finish'],
+        message: "Would you like to add another teammate?",
     },
 ];
 
-//Prompts
-const addTeammate = () =>{
-    return inquirer.prompt (addTeamMember)
-        .then(answer => {
-            switch(answer.addTeamMember){
-              case 'Engineer':
-                console.log('Engineer selected');
-                addEngineer();
-                break;
-              case 'Intern':
-                console.log('Intern selected'); 
-                addIntern();
-                break;
-              case 'Manager':
-                console.log ('Manager selected');
-                addManager();
-                break;
-            case 'Done':
-                writeFile(templateData(dataArr));
-                copyFile();
-                console.log('Input complete!')
+// question for engineer
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the team engineer's name?",
+        validate: (input) => {
+            return (!input) ? 'Please provide the name' : true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the team engineer's id?",
+        validate: (input) => {
+            if (isNaN(parseInt(input))) {
+                return 'Please enter an id number';
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the team engineer's email?",
+        validate: (email) => {
+            let valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            if (valid) {
+                return true;
+            }
+            return "Please enter a valid email";
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "What is the team engineer's github?",
+        validate: (input) => {
+            return (!input) ? 'Please provide the github' : true;
+        }
+    },
+];
 
+// question for intern
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the team intern's name?",
+        validate: (input) => {
+            return (!input) ? 'Please provide the name' : true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the team intern's id?",
+        validate: (input) => {
+            if (isNaN(parseInt(input))) {
+                return 'Please enter an id number';
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the team intern's email?",
+        validate: (email) => {
+            let valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            if (valid) {
+                return true;
+            }
+            return "Please enter a valid email";
+        }
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: "What is the team interns's school name?",
+        validate: (input) => {
+            return (!input) ? 'Please provide the school name' : true;
+        }
+    },
+];
+
+// check if the user want to add another teammate or finish the input.
+const addTeamOrFinish = () => {
+    return inquirer.prompt(addTeamOrFinishQuestion)
+        .then(answer => {
+            switch (answer.choice) {
+                case 'Engineer':
+                    // addEngineer();
+                    console.log('You chose Engineer');
+                    addEngineer();
+                    break;
+                case 'Intern':
+                    // addIntern();
+                    console.log('You chose Intern');
+                    addIntern();
+                    break;
+                case 'Finish':
+                    // here needs to be added function of creating a html with all the stuff.
+                    writeFile(templateData(dataArray));
+                    copyFile();
+                    console.log('You chose Finish. Check the dist folder');
+                    break;
             }
         })
 };
+
+// will add manager function to run questions and get answers and continue ask question until diced to stop will present a html page and a copy of the style.css 
+const addManager = () => {
+    //clean the array before start
+    dataArray.length = 0;
+    return inquirer.prompt(managerQuestions)
+        .then(answers => {
+            // destructure data by section
+            const { name, id, email, officeNumber } = answers;
+            const manager = new Manager(name, id, email, officeNumber);
+            dataArray.push(manager);
+            // console.log(manager);
+            addTeamOrFinish();
+        })
+};
+
+// add engineer function to run questions and get answers
 const addEngineer = () => {
     return inquirer.prompt(engineerQuestions)
         .then(answers => {
-            const {name,id,email,github} = answers;
+            // console.log(answers);
+            // destructure  data by section
+            const { name, id, email, github } = answers;
             const engineer = new Engineer(name, id, email, github);
-            dataArr.push(engineer);
-            addTeammate();
+            dataArray.push(engineer);
+            // console.log(engineer);
+            addTeamOrFinish();
         })
+
 };
+
+// add intern function to run questions and get answers
 const addIntern = () => {
     return inquirer.prompt(internQuestions)
         .then(answers => {
-            const {name,id,email,school} = answers;
-            const intern = new Intern(name, id, email,school);
-            dataArr.push(intern);
-            addTeammate();
-        })
-};
-const addManager = () => {
-    return inquirer.prompt(managerQuestions)
-        .then(answers => {
-            const {name,id,email,officeNumber} = answers;
-            const manager = new Manager(name, id, email, officeNumber);
-            dataArr.push(manager);
-            addTeammate();
+            // console.log(answers);
+            // destructure data by section
+            const { name, id, email, school } = answers;
+            const engineer = new Intern(name, id, email, school);
+            dataArray.push(engineer);
+            // console.log(engineer);
+            addTeamOrFinish();
         })
 }
+// Run the main function to build a html
+addManager();
 
-addEngineer();
+
+
---
+++ src/page-template.js
@@ -0,0 +1,92 @@
+// function to check right role and edit the template.
+const checkTheRole = (data) => {
+    switch (data.getRole()) {
+      case 'Manager':
+        return `Office: ${data.officeNumber}`;
+      case 'Engineer':
+        return `GitHub: <a href="https://github.com/${data.getGithub()}"
+        target="_blank">${data.getGithub()}</a>`;
+      case 'Intern':
+        return `School: ${data.getSchool()}`;
+    }
+  }
+  
+  // function to add icons for right role
+  const addIcon = (data) => {
+    switch (data.getRole()) {
+      case 'Manager':
+        return `<i class="fa-solid fa-people-roof"></i>`;
+      case 'Engineer':
+        return `<i class="fa-solid fa-brain"></i>`;
+      case 'Intern':
+        return `<i class="fa-solid fa-graduation-cap"></i>`;
+    }
+  };
+  
+  // create a card section
+  const generateCard = data => {
+    if (!data) {
+      return '';
+    }
+  
+    return `
+  <div class="card m-2 shadow-sm" id="" style="width: 18rem;">
+    <div class="card-header bg-primary text-white">
+        <h2 class="">${data.getName()}</h2>
+        <h3 class="">${data.getRole()} ${addIcon(data)}</h3>
+    </div>
+    <div class="card-body bg-light">
+        <ul class="list-group bg-white">
+            <li class="list-group-item">ID: ${data.getId()}</li>
+            <li class="list-group-item">Email: <a href="mailto:${data.getEmail()}">${data.getEmail()}</a>
+            </li>
+            <li class="list-group-item">${checkTheRole(data)}</li>
+        </ul>
+    </div>
+  </div>
+    `;
+  };
+  
+  
+  // create a html using bootstrap for css framework
+  const templateData = (dataArr) => {
+    // will store all the cards div 
+    let cards = ''
+    // iterate through the array of data we got after prompt
+    for (let i = 0; i < dataArr.length; i++) {
+      cards += generateCard(dataArr[i]);
+    }
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+        <nav class="navbar navbar-dark bg-danger text-center">
+            <span class="navbar-header mb-0 w-100 h1 text-white">My Team</span>
+        </nav>
+        <main class="container my-5">
+    
+            <div class="row justify-content-center">
+            ${cards}
+            </div>
+    
+        </main>
+    
+    </body>
+    
+    </html>
+    `;
+  };
+  
+  module.exports = templateData;
